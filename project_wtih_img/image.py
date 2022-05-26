b63a80fb6f882dc548ede703938ee59ae9a0c75d
YashkovDanila <danilaaskov1@gmail.com>
2022-05-26 17:37:30 +0300

    commentary execution

--- project_wtih_img/image.py
+++ project_wtih_img/image.py
@@ -1,26 +1,27 @@
 from PIL import Image, ImageDraw, ImageFont
 import random
-#(110, 61, 255)
+
 
 def get_position(digit, image_size, bounding_box):
     image_width, image_height = image_size
     left, top, right, bottom = bounding_box
-    if 0 <= digit < 25:
-        return (0, -top)
-    elif 25 <= digit < 50:
-        return (image_width - right, -top)
-    elif 50 <= digit < 75:
-        return (0, image_height - bottom)
-    else:
-        return (image_width - right, image_height - bottom)
+    match digit:
+        case num if 0 <= num <= 25:
+            return (0, -top)
+        case num if 25 < num <= 50:
+            return (image_width - right, -top)
+        case num if 50 < num <= 75:
+            return (0, image_height - bottom)
+        case _:
+            return (image_width - right, image_height - bottom)
 
 
 with Image.open('cow.png') as background:
-    digit = random.randint(0, 100)
+    digit = random.randint(0, 101)
     draw = ImageDraw.Draw(background)
     font = ImageFont.truetype("Roboto-Bold.ttf", 100)
     position = get_position(digit, background.size, font.getbbox(str(digit)))
-    draw.text(position, str(digit),tuple([random.randint(0,256) for i in range(3)]) , font=font)
+    draw.text(position, str(digit), tuple([random.randint(0, 256) for i in range(3)]), font=font)
     background.save('cow_edit.png')
 
 with Image.open('cow_edit.png') as image:
