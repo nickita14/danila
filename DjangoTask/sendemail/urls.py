85af799528d4549ee73885e736b1ae1bc7de27f4
YashkovDanila <danilaaskov1@gmail.com>
2022-06-10 11:39:42 +0300

    Delete useless files

--- DjangoTask/sendemail/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import *
-
-urlpatterns = [
-    path('', index, name='home'),
-    path('success/', success_view, name='success'),
-]
--- DjangoTask/sweet_shop/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import *
-
-urlpatterns = [
-    path('shop/', shop, name='shop'),
-    path('product/<slug:product_slug>', show_product, name='product'),
-    path('category/<slug:category_slug>', show_category, name='category'),
-]
